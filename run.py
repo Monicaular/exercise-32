acc22eb0b0042b29735ceb419affdd1d79f22d52
Monicaular <140078966+Monicaular@users.noreply.github.com>
2024-01-04 14:42:14 +0000

    Add function for showing whether the letter was guessed

--- run.py
+++ run.py
@@ -25,3 +25,17 @@ def ask_user_for_next_letter():
     """
     letter = input("Guess your letter: ")
     return letter.strip().upper()
+
+def generate_word_string(word, letters_guessed):
+    """
+    This function takes a word and a set of letters guessed.
+    It creates a string where guessed letters are shown, and unguessed letters are represented by underscores.
+    The resulting string is returned.
+    """
+    output = []
+    for letter in word:
+        if letter in letters_guessed:
+            output.append(letter.upper())
+        else:
+            output.append("_")
+    return " ".join(output)
